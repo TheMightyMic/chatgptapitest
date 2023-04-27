927ccf8d107ef3acce3ce1b824e6a6d767bc4ec2
mic <michisteinb@gmail.com>
2023-04-27 21:24:34 +0200

    refactoring

--- test.js
+++ test.js
@@ -1,39 +1,38 @@
-import {Configuration, OpenAIApi} from "openai";
-import * as readline from "readline";
+import { Configuration, OpenAIApi } from "openai";
+import readline from "readline";
 
 const configuration = new Configuration({
     apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
-let chatInterface = readline.createInterface({
+const chatInterface = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
+const messages = [{ role: "system", content: "Du bist Shakespear und antwortest nur in Reimen" }];
 
-let messages = [{role: "system", content: "Du bist Shakespear und antwortest nur in Reimen"}]
+async function processUserInput(input) {
 
-function processUserInput(input) {
+    messages.push({ role: "user", content: input });
 
-    messages.push({role: "user", content: input})
-
-    openai.createChatCompletion({
+    const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: messages,
-        max_tokens: 200
-    }).then(response => {
-        let answer = response.data.choices[0].message.content;
+        max_tokens: 200,
+    });
 
-        messages.push({role: "assistant", content: answer})
-        console.log(answer)
-        if (input.toLowerCase() === 'exit') {
-            console.log('Programm beendet.');
-            chatInterface.close();
-        } else {
-            chatInterface.question('', processUserInput);
-        }
-    })
+    const answer = response.data.choices[0].message.content;
+    messages.push({ role: "assistant", content: answer });
+    console.log(answer);
+
+    if (input.toLowerCase() === "exit") {
+        console.log("Programm beendet.");
+        chatInterface.close();
+    } else {
+        chatInterface.question('', processUserInput);
+    }
 }
 
 chatInterface.question('Was willst du wissen? ("exit" zum Beenden): ', processUserInput);
